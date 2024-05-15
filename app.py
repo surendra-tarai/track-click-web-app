9d0ad1bd7df7986bf23d38f018fdc44fd1d59567
Surendra Tarai <tarai.surendra@gmail.com>
2024-05-15 10:57:30 +0530

    capture ip address

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Home Page'
+    ip_address = request.remote_addr
+    
+    return f'Home Page <br> <br> Your IP address: {ip_address}'
 
 if __name__ == '__main__':
    app.run()
