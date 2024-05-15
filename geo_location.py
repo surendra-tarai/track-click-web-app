2b2c00d46c9aab9eb19ec5ae0db192e4dc6d27d8
Surendra Tarai <tarai.surendra@gmail.com>
2024-05-15 19:36:11 +0530

    handled error while fetching location

--- geo_location.py
+++ geo_location.py
@@ -7,20 +7,24 @@ reader = geoip2.database.Reader(DB_FILE_PATH)  # Path to your database file
 
 # Function to get geolocation data from IP address
 def get_geolocation(ip_address):
-    response = reader.city(ip_address)
-    
-    # Extract geolocation data
-    geolocation_data = {
-        'country': response.country.name,
-        'country_code': response.country.iso_code,
-        'city': response.city.name,
-        'latitude': response.location.latitude,
-        'longitude': response.location.longitude,
-    }
-    return geolocation_data
+    geolocation_data = {}
+    try:
+        response = reader.city(ip_address)
+        # Extract geolocation data
+        geolocation_data = {
+            'country': response.country.names,
+            'country_code': response.country.iso_code,
+            'city': response.city.name,
+            'latitude': response.location.latitude,
+            'longitude': response.location.longitude,
+        }
+    except Exception as e:
+        geolocation_data['error'] = str(e)
+    finally:
+        return geolocation_data
 
-# Example usage
-ip_address = '122.172.83.160'  # Replace with the actual IP address
-geolocation_data = get_geolocation(ip_address)
+# # Example usage
+# ip_address = '122.172.83.160'  # Replace with the actual IP address
+# geolocation_data = get_geolocation(ip_address)
 
-print(geolocation_data)
+# print(geolocation_data)
