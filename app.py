767fe5fcc573768d5fcb1af1791b708dee2d3091
Surendra Tarai <tarai.surendra@gmail.com>
2024-05-15 11:41:46 +0530

    capture both ipv4 and ipv6 address

--- app.py
+++ app.py
@@ -6,7 +6,15 @@ app = Flask(__name__)
 def home():
     ip_address = request.remote_addr
     
-    return f'Home Page <br> <br> Your IP address: {ip_address}'
+    ipv4_address = request.access_route[0] if request.access_route else None
+    ipv6_address = request.access_route[1] if len(request.access_route) > 1 else None
+
+    return f"""
+                Home Page <br> <br> 
+                Your IP address: {ip_address} <br> <br> 
+                Your ipv4_address: {ipv4_address} <br> <br> 
+                Your ipv6_address: {ipv6_address} <br> <br> 
+            """
 
 if __name__ == '__main__':
    app.run()
